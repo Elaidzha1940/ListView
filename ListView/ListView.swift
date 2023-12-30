ec2d7e35a24e8a0f7ea5a2bdc489c6dff113483f
Elaidzha1940 <eli080293@gmail.com>
2023-12-30 18:13:32 +0300

    ListView

---
+++ ListView/ListView.swift
@@ -0,0 +1,25 @@
+//  /*
+//
+//  Project: ListView
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 30.12.2023
+//
+//  */
+
+import SwiftUI
+
+struct ListView: View {
+    
+    
+    var body: some View {
+        
+        VStack {
+        
+        }
+    }
+}
+
+#Preview {
+    ListView()
+}
---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: ListView
+//  File: ListViewApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 30.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ListView()
+        }
+    }
+}
